aae0ded99c7a7d903905723b82a8ca3f76b3c743
Carlos Souza <carlos.pessoal@hotmail.com>
2023-11-25 03:28:04 -0300

    fix: use mysql2 driver over databasejs

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,22 +1,7 @@
 import mysql from "mysql2/promise";
 import { drizzle as mysqlDrizzle } from "drizzle-orm/mysql2";
-import { drizzle as planetscaleDrizzle } from "drizzle-orm/planetscale-serverless";
-
-import { Client } from "@planetscale/database";
 
 import { env } from "@/env";
-import * as schema from "./schema";
 
 const connection = await mysql.createConnection(env.DATABASE_URL);
-
-export const db =
-  env.NODE_ENV === "production"
-    ? planetscaleDrizzle(
-        new Client({
-          url: env.DATABASE_URL,
-        }).connection(),
-        {
-          schema,
-        },
-      )
-    : mysqlDrizzle(connection);
+export const db = mysqlDrizzle(connection);
