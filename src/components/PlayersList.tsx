01ac8f48f2d73571c13004c1bcd2cd450c737bde
Carlos Souza <carlos.pessoal@hotmail.com>
2023-11-25 01:55:26 -0300

    style: avatar height on players list

--- src/components/PlayersList.tsx
+++ src/components/PlayersList.tsx
@@ -11,14 +11,14 @@ export const PlayersList = () => {
     <div className="mt-4 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
       {data?.map(({ id, name, contract }) => (
         <Link href={`/player/${id}`} key={id}>
-          <div className="flex h-full flex-col items-center justify-center rounded bg-zinc-950 p-4">
+          <div className="flex h-full flex-col items-center justify-start rounded bg-zinc-950 p-4">
             <Avatar className="relative mb-4 h-16 w-16 overflow-visible">
               <AvatarFallback>{formatName(name)}</AvatarFallback>
-              <div className="absolute bottom-0 right-0 flex h-6 w-6 items-center justify-center rounded-full bg-zinc-600 text-sm ">
+              <div className="absolute bottom-0 right-0 flex h-6 w-6 items-center justify-center rounded-full bg-zinc-600 text-sm">
                 {contract.jerseyNumber}
               </div>
             </Avatar>
-            <p className="text-center text-sm font-medium">{name}</p>
+            <p className="h-full text-center text-sm font-medium">{name}</p>
           </div>
         </Link>
       ))}
