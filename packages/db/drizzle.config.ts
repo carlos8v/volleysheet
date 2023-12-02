bd32df19afb8e81f87e4651235e0a943e242efaa
Carlos Souza <carlos.pessoal@hotmail.com>
2023-12-02 12:25:59 -0300

    feat: update db scripts and deps

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,9 +1,4 @@
-import * as dotenv from "dotenv";
 import { type Config } from "drizzle-kit";
-
-dotenv.config({
-  path: "../../.env",
-});
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not set");
@@ -13,7 +8,7 @@ export default {
   schema: "./schema.ts",
   driver: "mysql2",
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL,
+    uri: process.env.DATABASE_URL,
   },
   tablesFilter: ["volleysheet_*"],
 } satisfies Config;
